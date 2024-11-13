ae79ba9de0b883d47f5690983cf0da3ef21fb92d
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-11-13 18:28:41 +0600

    optimized code

--- WEEK_03/day-3/Max_Sum_Subarray_of_size_K.cpp
+++ WEEK_03/day-3/Max_Sum_Subarray_of_size_K.cpp
@@ -1,21 +1,26 @@
 /*
 class Solution {
   public:
-    long long maximumSumSubarray(vector<int>& arr, int k) {
+    long long maximumSumSubarray(vector<int>& arr, int k) 
+    {
         // code here
-        long long int mx, ans = 0;
-        for(int i = 0; i < arr.size()-k+1; i++)
-        {
-            mx = 0;
-            int l = i+k;
-            for (int j = i; j < l; j++)
-            {
-                mx += arr[j];
-            }
-            // cout << mx <<nl;    
-            ans = max(ans, mx);
-        }
-        return ans;
+      long long mx = 0;
+      for (int i = 0; i < k; i++)
+      {
+         mx += arr[i];
+      }
+      
+      long long ans = mx;
+
+      for (int i = k, j = 0; i < arr.size(); i++, j++) 
+      {
+        // cout << mx << nl;
+         mx += arr[i];
+         mx -= arr[j];
+         ans = max(ans, mx);
+      }
+
+      return ans;
     }
 };
 */
