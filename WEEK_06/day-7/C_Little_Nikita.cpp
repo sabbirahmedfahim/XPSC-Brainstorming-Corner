4aa0c429c64afc4f818986a9830b742c1462df05
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-12-06 19:23:08 +0600

    day-7

---
+++ WEEK_06/day-7/C_Little_Nikita.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long int
+#define all(v) v.begin(),v.end()
+#define print(v) for(auto data : v) cout << data << " "; cout << nl
+using namespace std;
+void solve()
+{
+    int n, m; cin >> n >> m;
+    if(n < m) cout << "No" << nl;
+    else cout << (((n-m)%2 == 0)? "Yes" : "No") << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t; 
+    while (t--) solve();
+
+    return 0;
+}
