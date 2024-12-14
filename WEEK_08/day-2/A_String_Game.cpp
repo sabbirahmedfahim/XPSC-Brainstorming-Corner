e5def2283c6252ab87f1edea515639f85a0ac7bf
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-12-15 02:56:11 +0600

    day-2

---
+++ WEEK_08/day-2/A_String_Game.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long int
+#define all(v) v.begin(),v.end()
+#define print(v) for(auto data : v) cout << data << " "; cout << nl
+using namespace std;
+void solve()
+{
+    int n; string s; cin >> n >> s;
+    int one = 0, zero = 0;
+    for(auto data : s) data == '0'? zero++ : one++;
+    if(min(zero, one)%2) cout << "Zlatan" << nl;
+    else cout << "Ramos" << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t; 
+    while (t--) solve();
+
+    return 0;
+}
