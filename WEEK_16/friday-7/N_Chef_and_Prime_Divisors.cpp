73c722dede2e1809a99c56456ddd527717278a7b
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-02-21 01:48:26 +0600

    friday-7

---
+++ WEEK_16/friday-7/N_Chef_and_Prime_Divisors.cpp
@@ -0,0 +1,32 @@
+// resolved
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long int
+#define all(c) c.begin(),c.end()
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
+using namespace std;
+void solve()
+{
+    ll a, b; cin >> a >> b;
+    
+    while (__gcd(a, b) > 1)
+    {
+        b /= __gcd(a, b);
+    }
+    
+    if(b == 1) cout << "Yes" << nl;
+    else cout << "No" << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+    for(int tt = 1; tt <= t; tt++)
+    {
+        // cout << "TEST CASE-" << tt << nl;
+        solve();
+    }
+
+    return 0;
+}
